6d9592c7c8500426ffcc1f8a43c064bcfdd1a300
chungchung <hangjastar@gmail.com>
2025-05-03 21:32:31 +0900

    feat: init project

---
+++ src/main/kotlin/garlic/burger/BurgerApplication.kt
@@ -0,0 +1,11 @@
+package garlic.burger
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BurgerApplication
+
+fun main(args: Array<String>) {
+	runApplication<BurgerApplication>(*args)
+}
---
+++ src/test/kotlin/garlic/burger/BurgerApplicationTests.kt
@@ -0,0 +1,13 @@
+package garlic.burger
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BurgerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
