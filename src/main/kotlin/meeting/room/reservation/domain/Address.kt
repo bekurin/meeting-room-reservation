ed6e6ba39a0902db59fb946e1f107154a0a8bedb
chungchung <hangjastar@gmail.com>
2025-05-06 14:11:53 +0900

    feat: add init scope to address and timeperiod value

--- src/main/kotlin/meeting/room/reservation/domain/Address.kt
+++ src/main/kotlin/meeting/room/reservation/domain/Address.kt
@@ -2,6 +2,7 @@ package meeting.room.reservation.domain
 
 import jakarta.persistence.Column
 import jakarta.persistence.Embeddable
+import meeting.room.reservation.exception.BadRequestException
 
 @Embeddable
 data class Address(
@@ -11,4 +12,10 @@ data class Address(
     val street: String,
     @Column(nullable = false)
     val zipcode: String,
-)
+) {
+    init {
+        if (city.isBlank() || street.isBlank() || zipcode.isBlank()) {
+            throw BadRequestException("")
+        }
+    }
+}
--- src/main/kotlin/meeting/room/reservation/domain/TimePeriod.kt
+++ src/main/kotlin/meeting/room/reservation/domain/TimePeriod.kt
@@ -2,6 +2,8 @@ package meeting.room.reservation.domain
 
 import jakarta.persistence.Column
 import jakarta.persistence.Embeddable
+import meeting.room.reservation.exception.BadRequestException
+import java.time.Duration
 import java.time.LocalDateTime
 
 @Embeddable
@@ -10,4 +12,15 @@ data class TimePeriod(
     val startAt: LocalDateTime,
     @Column(nullable = false)
     val endAt: LocalDateTime,
-)
+) {
+    init {
+        if (startAt.isAfter(endAt)) {
+            throw BadRequestException("")
+        }
+
+        val betweenMinutes = Duration.between(startAt, endAt).toMinutes()
+        if (betweenMinutes < 10 || betweenMinutes > 480) {
+            throw BadRequestException("")
+        }
+    }
+}
